ab9ba6a567f19986f970b2ceec2f804bc2dde666
wahab-icp <wahab.icp@gmail.com>
2019-01-11 17:32:08 +0100

    new branch

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 __author__ = "abdul"
 
-#added a comment just to check
-#added comment from py2
-#added new comment
+#nolonger need comments
+def secondary():
+    print("new func")
 
+secondary()
 print("hello world")
 print("Hello world again")
+ 
